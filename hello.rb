fa7ae4ce6773ee678ef7d3a4276eff0a95680453
Jason Bucki <jasonbucki@gmail.com>
2012-08-15 22:25:08 -0500

    updating template

--- hello.rb
+++ hello.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
+template = <<-eos
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Texans Iframe Text</title>
+</head>
+<body style="background-color: black; text-align: center;">
+<iframe src="http://texans.fanchatter.com/" height="900" width="720"></iframe>
+</body>
+</html>
+eos
+
 get '/' do
-  "Hello World!"
+  template
 end
